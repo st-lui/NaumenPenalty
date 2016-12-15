cd696eed94187472880d469bf4881a70474c55f5
Eugeny Lutsenko <st.lui@live.ru>
2016-12-15 10:59:55 +0700

    Новый проект для изменения готового отчета из naumen

---
+++ ReportDownload/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportDownload
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
